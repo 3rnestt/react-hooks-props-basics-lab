5e5f0dadb24b76d1b8b5d7aec142d0632e7507e0
Ernest <ernest.wambugu@student.moringaschool.com>
2023-04-23 16:03:36 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,20 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  const { bio } = props;
+const isBio = bio && bio.trim() !== '';
   return (
     <div id="about">
       <h2>About Me</h2>
+      {isBio && <p>{bio}</p>}
       <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
+      {<Links
+      github= "https://github.com/liza"
+      linkedin="https://www.linkedin.com/in/liza/"
+      />}
+   </div>
   );
 }
 
